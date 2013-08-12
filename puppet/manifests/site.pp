24292c6c59183a14e62814390894298db51fdf35
Chris Timperley <christimperley@googlemail.com>
2013-08-12 16:09:51 +0100

    Added NPM and latest NodeJS to Puppetfile.

--- puppet/manifests/site.pp
+++ puppet/manifests/site.pp
@@ -1,12 +1,39 @@
 group { 'puppet': ensure => 'present' }
 
+# Global command path.
+Exec {
+  path => ['/bin/', '/sbin/', '/usr/bin/', '/usr/sbin/']
+}
+
 exec { 'apt-get update':
-  command => '/usr/bin/apt-get -y update'
+  command => 'apt-get -y update'
 }
 package { 'git':
   ensure => 'installed',
   require => Exec['apt-get update']
 }
 
-# Install Node.js
-class { 'nodejs': }
+# Install and configure Redis.
+class { 'redis':
+  version => '2.6.5',
+  #redis_port => '6379',
+  #redis_max_memory => '1gb',
+  require => Exec['apt-get update']
+}
+
+# Install Node.js and install NPM manifest.
+package { ['python-software-properties', 'python', 'g++', 'make']:
+  ensure => 'latest',
+  require => Exec['apt-get update']
+}
+exec { 'add-node-repo':
+  command => 'sh -c "add-apt-repository ppa:chris-lea/node.js; apt-get update"'
+}
+package { 'nodejs': 
+  ensure => 'latest',
+  require => Exec['add-node-repo']
+}
+exec { 'npm install': 
+  command => 'sh -c "cd /vagrant/app/node; npm install"',
+  require => [Package['nodejs'], Class['redis']]
+}
