80ec4261191a4337d2bd39c89e2ed7a874728b33
Chris Timperley <christimperley@googlemail.com>
2013-08-12 22:53:26 +0100

    Fixed express server setup to use "express()" instead of "express.createServer()".

--- app/node/server.js
+++ app/node/server.js
@@ -1,7 +1,7 @@
-var express = require('express');
-var app = express.createServer();
-var redis = require('redis');
-var io = require('socket.io').listen(app);
+var express = require('express')
+  , app = express()
+  , redis = require('redis')
+  , io = require('socket.io').listen(app);
 
 // Set Express to listen on port 8124.
 app.listen(8124);
