2ebb441ee4b1698f806c04295b6743bb10e822a1
Chris Timperley <christimperley@googlemail.com>
2013-08-12 14:04:04 +0100

    Added (manual) librarian-puppet setup and NodeJS installation to Puppet manifest.

---
+++ Gemfile
@@ -0,0 +1,7 @@
+source 'https://rubygems.org'
+
+gem 'puppet'
+
+# Required to fix issue with rc1 versions:
+# https://github.com/rodjek/librarian-puppet/issues/70
+gem 'librarian-puppet-maestrodev'
--- puppet/manifests/site.pp
+++ puppet/manifests/site.pp
@@ -1,27 +1,12 @@
-# Installs all required packages before starting real
-# installation.
-class install_requirements {
-  
-  group { 'puppet': ensure => 'present' }
-  exec { 'apt-get update':
-    command => '/usr/bin/apt-get -y update'
-  }
-  package { 'git':
-    ensure => 'installed',
-    require => Exec['apt-get update']
-  }
+group { 'puppet': ensure => 'present' }
 
+exec { 'apt-get update':
+  command => '/usr/bin/apt-get -y update'
+}
+package { 'git':
+  ensure => 'installed',
+  require => Exec['apt-get update']
 }
 
-# Main installer class.
-class install {
-
-  # Define stages.
-  stage { 'requirements':  }
-
-  # Associate classes to stages.
-  class { 'install_requirements': stage => 'requirements' }
-  
-}
-
-include install
+# Install Node.js
+class { 'nodejs': }
